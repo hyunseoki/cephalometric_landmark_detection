0485f9bc21f5e299fa8941a5fb23dc70666ad8cd
hyunseoki <hyunseoki@kaist.ac.kr>
2022-08-02 11:04:08 +0900

    move to scripts folder

---
+++ scripts/cuda1.ps1
@@ -0,0 +1,23 @@
+function PyScript{
+    param([String]$cmd)
+    $host.UI.RawUI.WindowTitle = $cmd
+    Invoke-Expression -Command $cmd
+}
+
+$device = "cuda:1"
+
+###############################################################################################
+$model = "SEUNet"
+$comments = $model + "_wo_activation"
+$save_folder = "./checkpoint/$comments"
+
+$train = "python src/main.py"  +
+            " --device $device " +
+
+            " --model $model" +
+            " --use_wandb True " +
+
+            " --save_folder $save_folder" +
+            " --comments $comments"
+
+PyScript($train)
